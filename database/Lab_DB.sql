db88e7c8d5002d62985d30e6e4d153c32eac1460
paterson <matus.petrovaj@gmail.com>
2024-05-25 12:54:38 +0200

    final_ref

--- database/Lab_DB.sql
+++ database/Lab_DB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hostiteľ: localhost:3306
--- Čas generovania: Št 02.Máj 2024, 17:24
+-- Čas generovania: So 25.Máj 2024, 10:50
 -- Verzia serveru: 10.11.4-MariaDB-1~deb12u1
 -- Verzia PHP: 8.2.7
 
@@ -20,11 +20,14 @@ SET time_zone = "+00:00";
 --
 -- Databáza: `Lab_DB`
 --
+CREATE DATABASE IF NOT EXISTS `Lab_DB` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `Lab_DB`;
 
 DELIMITER $$
 --
 -- Procedúry
 --
+DROP PROCEDURE IF EXISTS `ResetIncrement`$$
 CREATE DEFINER=`paterson`@`localhost` PROCEDURE `ResetIncrement` ()   BEGIN
     DECLARE currentVal INT;
     SELECT AUTO_INCREMENT INTO currentVal FROM information_schema.TABLES WHERE TABLE_SCHEMA = 'distance_db' AND TABLE_NAME = 'distance_table';
@@ -44,12 +47,13 @@ DELIMITER ;
 -- Štruktúra tabuľky pre tabuľku `bme_table`
 --
 
+DROP TABLE IF EXISTS `bme_table`;
 CREATE TABLE `bme_table` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `temperature` float DEFAULT NULL,
-  `humidity` float DEFAULT NULL,
-  `pressure` float DEFAULT NULL,
-  `timestamp` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+                             `id` bigint(20) UNSIGNED NOT NULL,
+                             `temperature` float DEFAULT NULL,
+                             `humidity` float DEFAULT NULL,
+                             `pressure` float DEFAULT NULL,
+                             `timestamp` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -58,10 +62,11 @@ CREATE TABLE `bme_table` (
 -- Štruktúra tabuľky pre tabuľku `distance_table`
 --
 
+DROP TABLE IF EXISTS `distance_table`;
 CREATE TABLE `distance_table` (
-  `id` bigint(20) NOT NULL,
-  `distance` int(11) DEFAULT NULL,
-  `timestamp` timestamp NULL DEFAULT current_timestamp()
+                                  `id` bigint(20) NOT NULL,
+                                  `distance` int(11) DEFAULT NULL,
+                                  `timestamp` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -70,10 +75,67 @@ CREATE TABLE `distance_table` (
 -- Štruktúra tabuľky pre tabuľku `gas_table`
 --
 
+DROP TABLE IF EXISTS `gas_table`;
 CREATE TABLE `gas_table` (
-  `id` bigint(20) NOT NULL,
-  `ppm` double DEFAULT NULL,
-  `timestamp` timestamp NULL DEFAULT current_timestamp()
+                             `id` bigint(20) NOT NULL,
+                             `ppm` double DEFAULT NULL,
+                             `timestamp` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Štruktúra tabuľky pre tabuľku `node_1_bme_table`
+--
+
+DROP TABLE IF EXISTS `node_1_bme_table`;
+CREATE TABLE `node_1_bme_table` (
+                                    `id` bigint(20) UNSIGNED NOT NULL,
+                                    `temperature` float DEFAULT NULL,
+                                    `humidity` float DEFAULT NULL,
+                                    `pressure` float DEFAULT NULL,
+                                    `timestamp` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Štruktúra tabuľky pre tabuľku `node_1_gas_table`
+--
+
+DROP TABLE IF EXISTS `node_1_gas_table`;
+CREATE TABLE `node_1_gas_table` (
+                                    `id` bigint(20) NOT NULL,
+                                    `ppm` double DEFAULT NULL,
+                                    `timestamp` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Štruktúra tabuľky pre tabuľku `node_2_bme_table`
+--
+
+DROP TABLE IF EXISTS `node_2_bme_table`;
+CREATE TABLE `node_2_bme_table` (
+                                    `id` bigint(20) UNSIGNED NOT NULL,
+                                    `temperature` float DEFAULT NULL,
+                                    `humidity` float DEFAULT NULL,
+                                    `pressure` float DEFAULT NULL,
+                                    `timestamp` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Štruktúra tabuľky pre tabuľku `node_2_gas_table`
+--
+
+DROP TABLE IF EXISTS `node_2_gas_table`;
+CREATE TABLE `node_2_gas_table` (
+                                    `id` bigint(20) NOT NULL,
+                                    `ppm` double DEFAULT NULL,
+                                    `timestamp` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -84,19 +146,43 @@ CREATE TABLE `gas_table` (
 -- Indexy pre tabuľku `bme_table`
 --
 ALTER TABLE `bme_table`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
 
 --
 -- Indexy pre tabuľku `distance_table`
 --
 ALTER TABLE `distance_table`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
 
 --
 -- Indexy pre tabuľku `gas_table`
 --
 ALTER TABLE `gas_table`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
+
+--
+-- Indexy pre tabuľku `node_1_bme_table`
+--
+ALTER TABLE `node_1_bme_table`
+    ADD PRIMARY KEY (`id`);
+
+--
+-- Indexy pre tabuľku `node_1_gas_table`
+--
+ALTER TABLE `node_1_gas_table`
+    ADD PRIMARY KEY (`id`);
+
+--
+-- Indexy pre tabuľku `node_2_bme_table`
+--
+ALTER TABLE `node_2_bme_table`
+    ADD PRIMARY KEY (`id`);
+
+--
+-- Indexy pre tabuľku `node_2_gas_table`
+--
+ALTER TABLE `node_2_gas_table`
+    ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT pre exportované tabuľky
@@ -106,28 +192,55 @@ ALTER TABLE `gas_table`
 -- AUTO_INCREMENT pre tabuľku `bme_table`
 --
 ALTER TABLE `bme_table`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+    MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pre tabuľku `distance_table`
 --
 ALTER TABLE `distance_table`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+    MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pre tabuľku `gas_table`
 --
 ALTER TABLE `gas_table`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+    MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pre tabuľku `node_1_bme_table`
+--
+ALTER TABLE `node_1_bme_table`
+    MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pre tabuľku `node_1_gas_table`
+--
+ALTER TABLE `node_1_gas_table`
+    MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pre tabuľku `node_2_bme_table`
+--
+ALTER TABLE `node_2_bme_table`
+    MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pre tabuľku `node_2_gas_table`
+--
+ALTER TABLE `node_2_gas_table`
+    MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 
 DELIMITER $$
 --
 -- Udalosti
 --
+DROP EVENT IF EXISTS `GasCleanupEvent`$$
 CREATE DEFINER=`paterson`@`localhost` EVENT `GasCleanupEvent` ON SCHEDULE EVERY 1 HOUR STARTS '2024-02-11 00:00:00' ON COMPLETION NOT PRESERVE ENABLE DO DELETE FROM gas_table WHERE timestamp < NOW() - INTERVAL 1 MONTH$$
 
+DROP EVENT IF EXISTS `DistanceCleanupEvent`$$
 CREATE DEFINER=`paterson`@`localhost` EVENT `DistanceCleanupEvent` ON SCHEDULE EVERY 1 HOUR STARTS '2024-02-11 00:00:00' ON COMPLETION NOT PRESERVE ENABLE DO DELETE FROM distance_table WHERE timestamp < NOW() - INTERVAL 1 MONTH$$
 
+DROP EVENT IF EXISTS `BmeCleanupEvent`$$
 CREATE DEFINER=`paterson`@`localhost` EVENT `BmeCleanupEvent` ON SCHEDULE EVERY 1 HOUR STARTS '2024-02-11 00:00:00' ON COMPLETION NOT PRESERVE ENABLE DO DELETE FROM bme_table WHERE timestamp < NOW() - INTERVAL 1 MONTH$$
 
 DELIMITER ;
